cc8b8982cc9447706fc0ee538e3d21279c26c770
Cunningham Li <snipercunni4399@gmail.com>
2026-03-23 16:24:40 -0400

    Services done

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCarSide, FaStar, FaShieldAlt, FaSprayCan, FaGem, FaMagic } from 'react-icons/fa';
+// import { FaCarSide, FaStar, FaShieldAlt, FaSprayCan, FaGem, FaMagic } from 'react-icons/fa';
 import { useLanguage } from '../App';
 import Reveal from './Reveal';
 import './Services.css';
@@ -7,32 +7,7 @@ import './Services.css';
 const Services = () => {
   const { t } = useLanguage();
   
-  const services = [
-    {
-      icon: <FaCarSide />,
-      data: t.services.service1
-    },
-    {
-      icon: <FaGem />,
-      data: t.services.service2
-    },
-    {
-      icon: <FaShieldAlt />,
-      data: t.services.service3
-    },
-    {
-      icon: <FaStar />,
-      data: t.services.service4
-    },
-    {
-      icon: <FaSprayCan />,
-      data: t.services.service5
-    },
-    {
-      icon: <FaMagic />,
-      data: t.services.service6
-    }
-  ];
+  const services = t.services.list;
 
   return (
     <section id="services" className="services">
@@ -52,10 +27,12 @@ const Services = () => {
           {services.map((service, index) => (
             <Reveal key={index} delay={index * 0.1}>
               <div className="service-card">
-                <div className="service-icon">{service.icon}</div>
-                <h3 className="service-title">{service.data.title}</h3>
-                <p className="service-description">{service.data.description}</p>
-                <p className="service-price">{service.data.price}</p>
+                <div className="service-image">
+                  <img src={service.image} alt={service.title} />
+                </div>
+                <h3 className="service-title">{service.title}</h3>
+                <p className="service-description">{service.description}</p>
+                <p className="service-price">{service.price}</p>
                 <button className="service-btn">{t.services.book}</button>
               </div>
             </Reveal>
